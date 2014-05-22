8abdf7bf0227f60b88c5056c5f4a07453219b46c
Alin Mindroc <mindroc.alin@gmail.com>
2014-05-22 10:43:31 +0300

    Update draw.py

--- draw.py
+++ draw.py
@@ -14,20 +14,20 @@ pygame.init()
 window = pygame.display.set_mode((640, 480))
 
 for line in data:
-	window.fill((0, 0, 0))
-	coordinates=[]
-	for i in line.split():
-			coordinates.append(map(int, i.split(':')))
+    window.fill((0, 0, 0))
+    coordinates=[]
+    for i in line.split():
+        coordinates.append(map(int, i.split(':')))
 
-	#desenez un patratel pentru fiecare pereche de coordonate din fisierul de intrare
-	for i in coordinates:
-		pygame.draw.rect(window, ((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))), pygame.Rect((10*i[0], -10*i[1]), w))
+    #desenez un patratel pentru fiecare pereche de coordonate din fisierul de intrare
+    for i in coordinates:
+        pygame.draw.rect(window, ((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))), pygame.Rect((10*i[0], -10*i[1]), w))
 
-	pygame.display.flip()
+    pygame.display.flip()
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit(0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
 
-	time.sleep(0.1)
+    time.sleep(0.1)
 
